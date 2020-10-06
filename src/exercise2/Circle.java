8d8ff877eaebc287059d11089eefa8fc4ba8e005
MarioMarkov <mario11septemvri@gmail.com>
2020-10-06 15:03:50 +0000

    ex1

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,19 @@
+package exercise2;
+import java.util.Scanner;
+
+
+public class Circle{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in)
+
+    double radius = input.nextInt();
+
+    double circ = 2.0*Math.PI()*radius;
+    double area = Math.PI* Math.pow(radius, 2);
+
+    System.out.println("Circumference: "+ circ);
+    System.out.println("Area: "+ area);
+
+
+  }
+}
