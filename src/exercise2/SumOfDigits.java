9ff49f212f12008cb271cefd74641da559a093c8
MarioMarkov <mario11septemvri@gmail.com>
2020-10-06 15:15:40 +0000

    ex2

---
+++ src/exercise2/SumOfDigits.java
@@ -0,0 +1,17 @@
+package exercise2;
+
+
+public class  SumOfDigits{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double digit = input.nextInt();
+
+        int a1 = digit%10;
+        int a2 = a1%10;
+        int
+        
+    }
+
+}
