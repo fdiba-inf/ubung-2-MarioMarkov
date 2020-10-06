adf2f6300f319b0f03dc039e36cde81987f83ec6
MarioMarkov <mario11septemvri@gmail.com>
2020-10-06 15:31:33 +0000

    ex3

--- src/exercise2/SumOfDigits.java
+++ src/exercise2/SumOfDigits.java
@@ -1,11 +1,22 @@
 package exercise2;
+import java.util.Scanner;
 
 
 public class  SumOfDigits{
 
     public static void main(String[] args) {
-        
-        
+        Scanner input = new Scanner(System.in);
+
+        int digit = input.nextInt();
+
+        int a1 = digit%10;
+        digit= digit/10;
+        int a2 = digit%10;
+        digit = digit/10;
+        int sum =a1+a2+digit;
+        System.out.println("Sum of digits: "+ sum);
+
+       
     }
 
 }
